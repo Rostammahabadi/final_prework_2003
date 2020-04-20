1bff8e31b7e14254d68c1dd327d21d961cafddb3
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-20 05:22:02 -1000

    add tests for department

--- test/department_test.rb
+++ test/department_test.rb
@@ -15,3 +15,26 @@ class DepartmentTest < MiniTest::Test
     assert_equal "Customer Service", customer_service.name
     assert_equal [], customer_service.employees
   end
+
+  def test_it_can_hire
+    customer_service = Department.new("Customer Service")
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})
+    customer_service.hire(bobbi)
+    customer_service.hire(aaron)
+    assert_equal [bobbi, aaron], customer_service.employees
+  end
+
+  def test_it_can_expense
+    customer_service = Department.new("Customer Service")
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})
+    customer_service.hire(bobbi)
+    customer_service.hire(aaron)
+    assert_equal 0, customer_service.expenses
+    customer_service.expense(100)
+    customer_service.expense(25)
+    assert_equal 125, customer_service.expenses
+  end
+
+end
