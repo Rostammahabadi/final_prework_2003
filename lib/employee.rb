e46215e3698fc3f27e739deb93ce0ff17980b496
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-20 05:19:37 -1000

    add employee class

---
+++ lib/employee.rb
@@ -0,0 +1,11 @@
+class Employee
+
+  attr_reader :name, :age, :salary
+
+  def initialize(info_hash)
+    @name = info_hash[:name]
+    @age = info_hash[:age].to_i
+    @salary = info_hash[:salary].to_i
+  end
+
+end
